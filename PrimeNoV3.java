4b0729f37a169f470506056d2d248af67d1b305e
rohinirao5724 <22bk1a1283@stpetershyd.com>
2025-01-24 11:29:31 +0530

    first commit

--- PrimeNoV3.java
+++ PrimeNoV3.java
@@ -1,28 +1,22 @@
 
-public class PrimeNoV2 {
+public class PrimeNoV3 {
 
 	public static void main(String[] args) {
-		for (int i=1,k=0;i<=100;i++) {
-			int num =i;
+		for(int i=1,k=0;i<=100;i++) {
+			int num=i;
 			int counter=0;
 			for(int p=1;p<=num;p++) {
 				if(num%p==0)counter++;
-				
 			}
-			if (counter==2) {
+			if(counter==2) {
 				System.out.print(num+" ");
-				k++;
-				if(k==6) {
+				if(++k %8 ==0) {
 					System.out.println();
-					k=0;
-					break;
-					
 				}
 			}
+				
 			
-		}
-		
-
 	}
 
+	}
 }
---
+++ PrimeNoV4.java
@@ -0,0 +1,9 @@
+
+public class PrimeNoV4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
