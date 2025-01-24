713c63e92bfb6c918eaa36438cfee44c09070562
rohinirao5724 <22bk1a1283@stpetershyd.com>
2025-01-24 12:21:03 +0530

    first commit

---
+++ Pattern.java
@@ -0,0 +1,12 @@
+
+public class Pattern{
+	public static void main(String[] args) {
+		for(int r=1;r<=5;r++) {
+			for(int c=5,s=1;s<=r;c--,s++) {
+				System.out.print(c+" ");
+			}
+			System.out.println();
+		}
+}		
+
+}
